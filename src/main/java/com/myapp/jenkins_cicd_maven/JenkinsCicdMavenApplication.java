4879952d8622c57983b124d6a26ac4c3eb03f27f
subha <subha@SUBHASHIS>
2024-06-22 11:28:04 +0530

    project skeleton

---
+++ src/main/java/com/myapp/jenkins_cicd_maven/JenkinsCicdMavenApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.jenkins_cicd_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsCicdMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsCicdMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myapp/jenkins_cicd_maven/JenkinsCicdMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.jenkins_cicd_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsCicdMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
